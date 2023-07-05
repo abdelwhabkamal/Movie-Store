c208f058242b5fc8d7ff419836ab8f371c4b61f6
Abdelwahab <hopakamal25@gmail.com>
2023-07-05 20:27:08 +0300

    ApplicationUser

---
+++ MovieStore/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieStore/Models/Domain/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MovieStore.Models.Domain
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string Name { get; set; }
+
+    }
+}
